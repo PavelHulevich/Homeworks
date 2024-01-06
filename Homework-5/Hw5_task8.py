b8037692bca844fa206741a85e0cb2de2b3b5acf
PavelHulevich <ferit.gomel@gmail.com>
2024-01-06 20:07:35 +0300

    Правка имен. Изменение формы условия

--- Homework-5/Hw5_task8.py
+++ Homework-5/Hw5_task8.py
@@ -1,5 +1,5 @@
 # Hw5_task8.py
-alphabet = {
+ALPHABET = {
     '.-  ': 'А', '-...': 'Б', '.-- ': 'В', '--. ': 'Г',
     '-.. ': 'Д', '.   ': 'Е', '...-': 'Ж', '--..': 'З',
     '..  ': 'И', '.---': 'Й', '-.- ': 'К', '.-..': 'Л',
@@ -14,22 +14,22 @@ alphabet = {
 def cleaning_morze(morze_noise_fn):
     morze_clean_fn = ''
     for char in morze_noise_fn:
-        if char == '-' or char == '.' or char == ' ':
+        if char in ['-', '.', ' ']:
             morze_clean_fn += char
     return morze_clean_fn
 
 
-def morze_to_text(morze_clean_fn):
+def converting_morze_to_text(morze_clean_fn):
     text_out_fn = ''
     for morze_pointer in range(0, len(morze_clean_fn), 4):
-        text_out_fn += alphabet[morze_clean_fn[morze_pointer:morze_pointer + 4]]
+        text_out_fn += ALPHABET[morze_clean_fn[morze_pointer:morze_pointer + 4]]
     return text_out_fn
 
 
 morze_noise = ('-   .   .sdfs.. Dfgh-   --- .-3467- 4'
                '--- .   ... +%--- --- лоууоу-...--.-.   -.  ..  .   ')  # телеграфное сообщение с помехами
 morze_clean = cleaning_morze(morze_noise)
-text_out = morze_to_text(morze_clean)
+text_out = converting_morze_to_text(morze_clean)
 print(f'{text_out}')
 print('%s' % text_out)
 print('{}'.format(text_out))
