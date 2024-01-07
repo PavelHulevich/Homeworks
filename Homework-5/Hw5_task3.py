aff43204d6709b42e64262ae31a56808c637473a
PavelHulevich <ferit.gomel@gmail.com>
2024-01-07 10:26:02 +0300

    В цикл добавлен enumerate(sentences_list) Изменения в функции вывода на экран

--- Homework-5/Hw5_task3.py
+++ Homework-5/Hw5_task3.py
@@ -8,15 +8,15 @@
 
 def print_quantity_words(sent_nmb, word_cnt):  # Вывод номера предложения и количества слов в нем
     print(f'Вывод форматированием f-строкой:    Sentence {sent_nmb} has {word_cnt} words.')
-    print('Вывод форматированием ' + chr(37) + '-способом:   Sentence %s has %s words' % (sent_nmb, word_cnt))
+    print('Вывод форматированием %s-способом:   Sentence %s has %s words' % ('%', sent_nmb, word_cnt))
     print('Вывод форматированием через format: Sentence {} has {} words\n'.format(sent_nmb, word_cnt))
 
 
 string_in = ('Python is an easy to learn language. It has many applications in various fields. '
              'You can use it for web development, data analysis, machine learning, and more.')
-sentences_list = string_in.split('. ')                    # Список предложений из строки
-for sentence in sentences_list:                           # Перебираем все предложения из списка предложений
-    sentence_number = sentences_list.index(sentence) + 1  # Порядковый номер текущего предложения
+sentences_list = string_in.split('.')[0:-1]               # Список предложений из строки
+for i, sentence in enumerate(sentences_list):             # Перебираем все предложения из списка предложений
+    sentence_number = i + 1                               # Порядковый номер текущего предложения
     words_list = sentence.split(' ')                      # Список слов из текущего предложения
     words_count = len(words_list)                         # Количество слов в текущем предложении
     print_quantity_words(sentence_number, words_count)
