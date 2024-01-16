ec4522620cbc8fd739808467cc33a2ff4c268bbd
PavelHulevich <ferit.gomel@gmail.com>
2024-01-16 11:43:57 +0300

    Добавлены тестовые прогоны

--- Homework-7/Hw7_test16.py
+++ Homework-7/Hw7_test16.py
@@ -8,8 +8,34 @@
 Множество букв, которые не входят в строку: {'a', 'b', 'c', 'f', 'g', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 's', 't', 'u',
 'v', 'x', 'y', 'z'}
 """
+from random import randint
+from time import sleep
+
+def finding_different_set(string) -> set:
+    alphabet_out = {(lambda i: chr(i))(i) for i in range(97, 123) if chr(i) not in string}
+    return alphabet_out
 
 
-list_in = "Hello, world!"
-alphabet_out = {(lambda i: chr(i))(i) for i in range(97, 123) if chr(i) not in list_in}
-print(alphabet_out)
+def validate_enter_data(string: str) -> bool:
+    # Проверка валидности исходных данных. На выходе False - если ошибка входных данных.
+    if not isinstance(string, str):
+        print('Ошибка. На входе не строка')
+        return False
+    return True  # No errors
+
+
+def finding_different_set_enter(string: str):
+    validate_result = validate_enter_data(string)
+    if validate_result:
+        print(f'Введена строка: "{string}"\n '
+              f' множество символов алфавита за минусом входящих в строку: {finding_different_set(string)}\n')
+    else:
+        print(f'Ошибка, в аргументе: "{string}" входные данные не верны (не строка)\n')
+
+
+# Тестовые прогоны
+tests_list = ["Hello, world!",  "vending machine", 'drinks', 25, ['machine', 'vending'], {25, 35, 75}, 'a']
+while True:
+    test_index = randint(0, len(tests_list)-1)
+    finding_different_set_enter(tests_list[test_index])
+    sleep(3)
