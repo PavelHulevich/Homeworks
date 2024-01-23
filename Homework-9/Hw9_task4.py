9c6ded7e190aae8ac999c899ac525a8c35760300
PavelHulevich <ferit.gomel@gmail.com>
2024-01-23 08:57:33 +0300

    Домашняя работа - 9

---
+++ Homework-9/Hw9_task4.py
@@ -0,0 +1,52 @@
+"""Задание 4:
+Напишите функцию count_vowels(s: str, case_sensitive: bool = False) -> int, которая возвращает
+количество уникальных гласных букв в строке s. Гласными буквами считаются 'a', 'e', 'i', 'o',
+'u'. Аргумент case_sensitive определяет, учитывать ли регистр букв при подсчете. Если
+case_sensitive равен False, то функция должна игнорировать регистр букв и считать 'A' и 'a'
+одинаковыми. Если case_sensitive равен True, то функция должна различать регистр букв и
+считать 'A' и 'a' разными. Например, count_vowels("Hello, world!", case_sensitive=True) должна
+вернуть 2, так как в строке есть только две гласные буквы 'e' и 'o'.
+"""
+from random import choice
+from time import sleep
+
+
+def count_vowels(string_in: str, case_sensitive: bool = False) -> int:
+    vowels_low = ['a', 'e', 'i', 'o', 'u']
+    vowels_caps = ['A', 'E', 'I', 'O', 'U']
+    if case_sensitive:
+        vowels_set = {i for i in string_in if i in vowels_low or i in vowels_caps}
+    else:
+        vowels_set = {i.lower() for i in string_in if i in vowels_low or i in vowels_caps}
+    return len(vowels_set)
+
+
+def is_validate_data(string_in: str, flag) -> bool:
+    # Проверка валидности исходных данных. На выходе False - если ошибка входных данных.
+    if not isinstance(string_in, str):
+        print('Ошибка. На входе не строка')
+        return False
+    if not isinstance(flag, bool):
+        print('Ошибка. Второй аргумент не логический')
+        return False
+    return True  # No errors
+
+
+def count_vowels_entry(string_in: str, case_sensitive: bool = False) -> None:
+    print(f'\nВведены данные : "{string_in}", {case_sensitive}. Регистр букв ', end='')
+    if case_sensitive:
+        print('РАЗЛИЧАЕТСЯ')
+    else:
+        print('НЕ РАЗЛИЧАЕТСЯ')
+    if is_validate_data(string_in, case_sensitive):
+        print('Количество гласных букв во введенной строке: ', count_vowels(string_in, case_sensitive))
+    else:
+        print('Не верные входные данные')
+
+
+TEST_LIST = ["Hello, world!", "Hello, WORLD!", 'map_square', 'from time import sleep', 'from time IMPORT SLEEP',
+             'case_sensitive', 'case_SENSITIVE', 23, [15, 25], ['say', 45], {4, 5}]
+while True:
+    count_vowels_entry(choice(TEST_LIST), choice([True, False, 'case_SENSITIVE', 23, [15, 25], ['say', 45],
+                                                  True, False, True, False, {4, 5}]))
+    sleep(3)
--- Notes/raw_test.py
+++ Notes/raw_test.py
@@ -1,1 +1,8 @@
-print(bin(0))
+new_array=[1,2,3,4]
+power_set=[[]]
+for x in new_array:
+    for i in range(len(power_set)):
+        tmp_list = power_set[i].copy()
+        tmp_list.append(x)
+        power_set.append(tmp_list)
+print(power_set)
