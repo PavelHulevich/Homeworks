ebf48dd8dbf63669a82afc493cc62b6ff5dff281
PavelHulevich <ferit.gomel@gmail.com>
2024-01-24 15:43:51 +0300

    Домашняя работа - 9

---
+++ Homework-9/Hw9_task8.py
@@ -0,0 +1,56 @@
+"""Задание 8*:
+Напишите функцию для вычисления суммы чисел списка с поддержкой вложенных списков с
+помощью рекурсии. Программа должна выбрасывать исключение в случае, если в списке
+будет встречаться не числа."""
+"""Задание 5:
+Напишите функцию для вычисления суммы чисел списка с поддержкой вложенных списков с
+помощью рекурсии."""
+from time import sleep
+import sys
+
+def calc_sum_list(list_in: list) -> int:
+    # Вычисление суммы всех элементов списка, включая элементы всех вложенных списков (рекурсия).
+    if isinstance(list_in[0], list):
+        list_in[0] = calc_sum_list(list_in[0])
+    if len(list_in) < 2:
+        return list_in[0]
+    else:
+        try:
+            return list_in[0] + calc_sum_list(list_in[1:])
+        except TypeError:
+            print(f'СРАБОТАЛО ИСКЛЮЧЕНИЕ: {sys.exc_info()[0]}  В списке содержатся не число')
+            return 0
+
+
+def is_validate_data(list_in: list) -> bool:
+    # Проверка валидности исходных данных. На выходе False - если ошибка входных данных.
+    if not isinstance(list_in, list):
+        print('Ошибка. На входе не список')
+        return False
+    for index in list_in:
+        if isinstance(index, list):
+            if not is_validate_data(index):
+                print('Вложенный список содержит недопустимые данные')
+                return False
+            else:
+                continue
+        # if not isinstance(index, int):
+        #     print('В списке содержится не целое число')
+        #     return False
+    return True
+
+
+def calc_sum_list_entry(list_in: list) -> None:
+    print(f'\nВведены денные: {list_in}')
+    if is_validate_data(list_in):
+        result = calc_sum_list(list_in)
+        print(f'Сумма всех значений в списке и всех вложенных списках: {result}')
+    else:
+        print('Неверные входные данные')
+
+
+TEST_LIST = [[2, 2, 2, [3, 5, 6, [3, 5]], [3, 5]], [3, 5, 6, [3, 5]], [3, 5, 6, ], 25, [[[3, 4], [3, 4]], [1, 2]],
+             'say', {2, 3}, 122, False, [[3, 5], [3, 5]], [[3, 'a'], [3, 5]], [[3, 5], ['day', 5]]]
+for index in TEST_LIST:
+    calc_sum_list_entry(index)
+    sleep(1)
--- Notes/raw_test.py
+++ Notes/raw_test.py
@@ -1,55 +1,12 @@
-# """Задание 7:
-# Напишите функцию modify_list(lst: list, *funcs: callable) -> None, которая принимает в
-# качестве аргументов список lst и произвольное количество функций funcs, и модифицирует
-# список lst так, что каждый его элемент преобразуется с помощью всех функций из funcs по
-# порядку. Функции из funcs должны принимать один аргумент и возвращать одно значение.
-# Функция modify_list не должна возвращать ничего, а изменять список lst на месте. Например,
-# modify_list([1, 2, 3], lambda x: x + 1, lambda x: x * 2) должна изменить список [1, 2, 3] на [4, 6,
-# 8], так как каждый элемент сначала увеличивается на 1, а потом умножается на 2. Функции
-# для модификации элеметов списка:
-# • Возведение в квадрат четных чисел
-# • Увеличение на 1 нечетных чисел
-# • Умножение на 3 простых чисел"""
-#
-#
-# def modify_list(a: int, *funcs: callable) -> None:
-#     funcs[1](10)
-#     funcs[0](10)
-#
-#
-# # def square_of_even(number):
-# #     print(1)
-# #
-# # def increment_of_odd():
-# #     print(2)
-# #
-# # TEST_LIST = [2, 4, 6, 10]
-# def test_func(b):
-#     print(b*3)
-#
-#
-# def test_func2(b):
-#     print(b/2)
-#
-# modify_list(3, test_func, test_func2)
-
-print(6 % 3)
-
-
-
-# def is_validate_data(list_in: list) -> bool:
-#     # Проверка валидности исходных данных. На выходе False - если ошибка входных данных.
-#     if not isinstance(list_in, list):
-#         print('Ошибка. На входе не список')
-#         return False
-#     for number in list_in:
-#         if not isinstance(number, int):
-#             print('Ошибка. В списке не целые числа')
-#             return False
-#     return True  # No errors
-
-
-def modify_list(lst: list, *funcs: callable) -> None:
-    # print('\nВведены данные: ', lst)
-    # if not is_validate_data(lst):
-    #     print('На входе неверные значения')
+import sys
+randomList = ['a', 0, 2]
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)  #  Если исключений не возникает, блок except пропускается
+                          #  программа продолжает выполнятся обычным образом
+        print(entry, r)
+    except:
+        print("Oops!", sys.exc_info()[0], "occurred.")
+        print("Next entry.")
+        print()
