bba13711bf67dcc984711089e13b56a5ca1d4f5f
PavelHulevich <ferit.gomel@gmail.com>
2024-02-13 14:01:04 +0300

    Домашняя работа - 15

--- Homework-15/simple_app/hello/views.py
+++ Homework-15/simple_app/hello/views.py
@@ -2,9 +2,10 @@
 #
 # Create your views here.
 from django.http import HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
 
 
-def hello_view(request):
+def hello_view(request: WSGIRequest):
     name = request.GET.get("name", "World")
     message = request.GET.get("message", "Have a nice day")
 
