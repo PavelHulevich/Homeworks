6a7098677ad26c2600dcf907b116f42728c99a03
PavelHulevich <ferit.gomel@gmail.com>
2025-10-24 13:50:00 +0300

    Stepik_Homeworks

---
+++ Stepik_Homeworks 6_2/SH6_2_task_1.py
@@ -0,0 +1,8 @@
+# Напишите программу, которая выводит текст:
+#
+# "Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+#
+#
+# Примечание. Используйте конкатенацию строк.
+
+print('"Python is a great language!", said Fred. "I don'+"'"+'t ever remember having this much fun before."')
