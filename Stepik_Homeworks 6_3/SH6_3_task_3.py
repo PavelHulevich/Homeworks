5f9b10ac40e31553d2b141b877d97d18d213ad16
PavelHulevich <ferit.gomel@gmail.com>
2025-10-24 14:28:36 +0300

    Stepik_Homeworks

--- Stepik_Homeworks 6_3/SH6_3_task_3.py
+++ Stepik_Homeworks 6_3/SH6_3_task_3.py
@@ -109,4 +109,4 @@ from math import sqrt
 
 x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
 
-print(sqrt((x1 - x2)**2 + (y1 - y2)**2 ))
+print(sqrt(( x1 - x2)**2 + (y1 - y2)**2 ))
